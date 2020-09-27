77bce4aaf08437081b7916efd96b93dd25e0cb3c
semih <semihkirdinli@gmail.com>
2020-09-27 14:42:24 +0300

    Starter class name has been changed as Application.java

--- src/main/java/com/challenge/poll/Application.java
+++ src/main/java/com/challenge/poll/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PollApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PollApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
